c2d9ae4eb6ad6f22e1c186886fb45d0ef3256ee2
mathsencud-creator <mathsencud@gmail.com>
2026-01-04 07:14:46 -0800

    Change target process from Mechvibes to Rundll

--- wou.ps1
+++ wou.ps1
@@ -7,7 +7,7 @@ if (-not (Test-Path $dllPath)) {
 }
 
 
-$processName = "Mechvibes"
+$processName = "Rundll"
 $process = Get-Process -Name $processName -ErrorAction SilentlyContinue
 if (-not $process) {
  
@@ -97,6 +97,7 @@ if ($result) {
 } else {
   
 }
+
 
 
 
